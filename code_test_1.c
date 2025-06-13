ed59320bb8f27ab655cd0560238549253af956bd
McKinnon <William.P.McKinnon@GD-MS.US>
2025-06-13 11:28:27 -0400

    New revision for the first test

--- code_test_1.c
+++ code_test_1.c
@@ -1,21 +1,27 @@
+
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    int password;
-    int password_attempt;
+    char username[50];
+    char password[50];
+    char password_attempt[50];
     
+    printf("Create a username:  ");
+    scanf("%49s", username);
     printf("Create a password: ");
-    scanf("%d", &password);
+    scanf("%49s", password);
     
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; i++) {
         printf("Enter your password: ");
-        scanf("%d", &password_attempt);
+        scanf("%49s", password_attempt);
         
-        if (password_attempt != password){
+        if (strcmp(password_attempt, password) != 0) {
             printf("Wrong! Try Again.\n");
         } else {
             printf("Accepted, loading profile\n");
+            printf("Hello, %s\n", username);
             break;
         }
     }
